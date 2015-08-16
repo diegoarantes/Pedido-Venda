e4f09005aa411a0554eaf687154275f60bcbab65
Diego Arantes <Diego Arantes@DIEGO-NOT>
2015-08-16 11:09:51 -0300

    Implantação de Validações

--- src/main/java/com/absoft/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/com/absoft/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,9 +1,14 @@
 package com.absoft.pedidovenda.controller;
 
+import com.absoft.pedidovenda.model.Categoria;
 import com.absoft.pedidovenda.model.Produto;
 import java.io.Serializable;
+import java.util.List;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -17,8 +22,20 @@ public class CadastroProdutoBean implements Serializable {
 
     private Produto produto;
 
+    private List<Categoria> categoriaRaizes;
+
     public CadastroProdutoBean() {
         produto = new Produto();
+    }
+
+    public void inicializar() {
+        System.out.println("Inicializando ....");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("PedidoPU");
+        EntityManager manager = factory.createEntityManager();
+
+        categoriaRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
+
+        manager.close(); //Fechar Entity Manager
     }
 
     public void salvar() {
@@ -27,6 +44,14 @@ public class CadastroProdutoBean implements Serializable {
 
     public Produto getProduto() {
         return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public List<Categoria> getCategoriaRaizes() {
+        return categoriaRaizes;
     }
 
 }
